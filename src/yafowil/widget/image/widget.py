0efe9cfc2bdb50917c4b85820bfcc92e1d9d825b
Robert Niederreiter <rnix@squarewave.at>
2012-04-20 14:41:50 +0200

    stub

--- src/yafowil/widget/image/widget.py
+++ src/yafowil/widget/image/widget.py
@@ -10,12 +10,24 @@ from yafowil.utils import (
     css_managed_props,
     managedprops,
 )
-from yafowil.common import generic_required_extractor
+from yafowil.common import (
+    generic_required_extractor,
+    file_extractor,
+    input_file_edit_renderer,
+    file_options_renderer,
+)
 
 
 @managedprops('image', *css_managed_props)
 def image_edit_renderer(widget, data):
-    return data.rendered
+    if widget.attrs['url']:
+        tag = data.tag
+        img_attrs = {
+            'src': widget.attrs['src'],
+            'alt': widget.attrs['alt'],
+        }
+        img = tag('img', **img_attrs)
+    return img + data.rendered
 
 
 def image_extractor(widget, data):
@@ -23,13 +35,21 @@ def image_extractor(widget, data):
 
 
 def image_display_renderer(widget, data):
-    return '<div>Image</div>'
+    if widget.attrs['url']:
+        tag = data.tag
+        img_attrs = {
+            'src': widget.attrs['src'],
+            'alt': widget.attrs['alt'],
+        }
+        return tag('img', **img_attrs)
+    return data.rendered
 
 
 factory.register(
     'image',
-    extractors=[image_extractor, generic_required_extractor],
-    edit_renderers=[image_edit_renderer],
+    extractors=[image_extractor, file_extractor, generic_required_extractor],
+    edit_renderers=[
+        image_edit_renderer, input_file_edit_renderer, file_options_renderer],
     display_renderers=[image_display_renderer])
 
 factory.doc['blueprint']['image'] = \
@@ -44,3 +64,21 @@ factory.defaults['image.required'] = False
 factory.defaults['image.error_class'] = 'error'
 
 factory.defaults['image.message_class'] = 'errormessage'
+
+factory.defaults['image.accept'] = 'image/*'
+
+factory.defaults['image.vocabulary'] = [
+    ('keep', u'Keep Existing image'),
+    ('replace', u'Replace existing image'),
+    ('delete', u'Delete existing image'),
+]
+
+factory.defaults['image.alt'] = ''
+factory.doc['props']['image.alt'] = """\
+Image alternative text.
+"""
+
+factory.defaults['image.src'] = None
+factory.doc['props']['image.src'] = """\
+Image URL if image present and displaying is desired.
+"""
