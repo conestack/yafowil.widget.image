0be185f53c91342fbaf992988646af8c4096d3d9
Jens W. Klein <jens@bluedynamics.com>
2012-06-13 00:23:46 +0200

    refactored examples

--- src/yafowil/widget/image/example.py
+++ src/yafowil/widget/image/example.py
@@ -1,7 +1,6 @@
 from yafowil.base import factory
 
-
 def get_example():
     part = factory(u'fieldset', name='yafowilwidgetimage')
     part['image'] = factory('field:label:error:image', props={})
-    return {'widget': part, 'routes': {}}
+    return [{'widget': part, 'doc': 'TODO'}]
