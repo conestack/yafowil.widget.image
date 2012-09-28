b4eefc6bc4428c812053aee5b752cc589020ce94
Robert Niederreiter <office@squarewave.at>
2012-09-28 10:50:36 +0200

    blanklines

--- src/yafowil/widget/image/__init__.py
+++ src/yafowil/widget/image/__init__.py
@@ -2,15 +2,12 @@ import os
 from yafowil.base import factory
 
 
-# XXX: use fanstatic
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
-
 js = [{
     'group': 'yafowil.widget.image',
     'resource': 'widget.js',
     'order': 20,
 }]
-
 css = [{
     'group': 'yafowil.widget.image',
     'resource': 'widget.css',
