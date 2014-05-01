7f0f15df6a1244b3f69a7fa34d8de4fbb6ffa529
Robert Niederreiter <office@squarewave.at>
2014-05-01 09:36:57 +0200

    "package maintenance"

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(name='yafowil.widget.image',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'yafowil>1.99',
+          'yafowil>2.0.99',
           'ImageUtils',
       ],
       tests_require=tests_require,
