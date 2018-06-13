2eac90c019c0cc0b8479d820514de3e6a261c569
Georg Gogo. Bernhard <gogo@bluedynamics.com>
2018-06-13 09:04:01 +0200

    Python3 compatibility

--- src/yafowil/widget/image/tests.py
+++ src/yafowil/widget/image/tests.py
@@ -1,4 +1,8 @@
-from StringIO import StringIO
+from yafowil.compat import IS_PY2
+if IS_PY2:
+    from StringIO import StringIO
+else:
+    from io import StringIO
 from decimal import Decimal
 from node.utils import UNSET
 from yafowil.base import ExtractionError
@@ -9,7 +13,7 @@ from yafowil.widget.image.utils import aspect_ratio_approximate
 from yafowil.widget.image.utils import same_aspect_ratio
 from yafowil.widget.image.utils import scale_size
 import PIL
-import os
+# import os
 import pkg_resources
 import unittest
 import yafowil.loader
